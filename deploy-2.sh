593dd32df0c545de7a413f86d0a5eacbc2d48e94
ykab <y7a.kab@gmail.com>
2022-04-22 15:23:56 +0900

    パラメーターのネーミングの見直し、インデント修正など。

--- deploy-2.sh
+++ deploy-2.sh
@@ -8,16 +8,17 @@
 set -x
 
 # export AWS_COGNITO_REGION=$AWS_DEFAULT_REGION
+
 [ -z "$STACK_NAME" ] && echo "Please specify STACK_NAME environment variable" && exit 1;
 
 export AWS_USER_POOLS_WEB_CLIENT_ID=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='CognitoClientID'].OutputValue" --output text`
 [ -z "$AWS_USER_POOLS_WEB_CLIENT_ID" ] && echo "Can not retrive CognitoClientID." && exit 1;
 
-export API_BASE_URL=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='TodoFunctionApi'].OutputValue" --output text`
-[ -z "$API_BASE_URL" ] && echo "Can not retrive TodoFunctionApi." && exit 1;
+export API_BASE_URL=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='TodoApiUrl'].OutputValue" --output text`
+[ -z "$API_BASE_URL" ] && echo "Can not retrive TodoApiUrl." && exit 1;
 
-export STAGE_NAME_PARAM=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Parameters[?ParameterKey=='APIGatewayStageName'].ParameterValue" --output text`
-[ -z "$STAGE_NAME_PARAM" ] && echo "Can not retrive APIGatewayStageName." && exit 1;
+export STAGE_NAME_PARAM=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Parameters[?ParameterKey=='StageName'].ParameterValue" --output text`
+[ -z "$STAGE_NAME_PARAM" ] && echo "Can not retrive StageName." && exit 1;
 
 export COGNITO_HOSTED_DOMAIN=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='CognitoDomainName'].OutputValue" --output text`
 [ -z "$COGNITO_HOSTED_DOMAIN" ] && echo "Can not retrive CognitoDomainName." && exit 1;
@@ -29,13 +30,13 @@ cp www/src/config.default.js www/src/config.js
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' -e 's/AWS_USER_POOLS_WEB_CLIENT_ID/'"$AWS_USER_POOLS_WEB_CLIENT_ID"'/g' www/src/config.js
   sed -i '' -e 's/API_BASE_URL/'"${API_BASE_URL//\//\\/}"'/g' www/src/config.js
-  sed -i '' -e 's/{APIGatewayStageName}/'"$STAGE_NAME_PARAM"'/g' www/src/config.js
+  sed -i '' -e 's/{StageName}/'"$STAGE_NAME_PARAM"'/g' www/src/config.js
   sed -i '' -e 's/COGNITO_HOSTED_DOMAIN/'"$COGNITO_HOSTED_DOMAIN"'/g' www/src/config.js
   sed -i '' -e 's/REDIRECT_URL/'"${REDIRECT_URL//\//\\/}"'/g' www/src/config.js
 else
   sed -i -e 's/AWS_USER_POOLS_WEB_CLIENT_ID/'"$AWS_USER_POOLS_WEB_CLIENT_ID"'/g' www/src/config.js
   sed -i -e 's/API_BASE_URL/'"${API_BASE_URL//\//\\/}"'/g' www/src/config.js
-  sed -i -e 's/{APIGatewayStageName}/'"$STAGE_NAME_PARAM"'/g' www/src/config.js
+  sed -i -e 's/{StageName}/'"$STAGE_NAME_PARAM"'/g' www/src/config.js
   sed -i -e 's/COGNITO_HOSTED_DOMAIN/'"$COGNITO_HOSTED_DOMAIN"'/g' www/src/config.js
   sed -i -e 's/REDIRECT_URL/'"${REDIRECT_URL//\//\\/}"'/g' www/src/config.js
 fi
